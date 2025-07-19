f5ca6c84c1141ad94be0ac97a8e36d4b4918f2f2
sladereaperr <karanjadhav2003@gmail.com>
2025-07-19 08:27:45 +0530

    Added Title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import { Toaster } from "sonner";
+
+// Add this metadata object
+export const metadata: Metadata = {
+  title: "Karan's Portfolio", // <-- ADD YOUR TITLE HERE
+  description: "Portfolio of a final year student of PES University",
+};
 
 export default function RootLayout({
   children,
